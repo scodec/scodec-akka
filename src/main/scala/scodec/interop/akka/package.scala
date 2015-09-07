687fdcc77c1918f0a818f4886aa26b1bcc63085a
Michael Pilquist <mpilquist@gmail.com>
2015-09-07 17:29:24 -0400

    Zero-copy, though often easily beatable, implementation of ByteString => ByteVector

--- src/main/scala/scodec/interop/akka/package.scala
+++ src/main/scala/scodec/interop/akka/package.scala
@@ -7,7 +7,7 @@ import _root_.akka.util.ByteString
 package object akka {
 
   implicit class EnrichedByteString(val value: ByteString) extends AnyVal {
-    def toByteVector: ByteVector = ByteVector.view(value.asByteBuffer)
+    def toByteVector: ByteVector = ByteVector.view(idx => value(idx), value.size)
   }
 
   implicit class EnrichedByteVector(val value: ByteVector) extends AnyVal {
